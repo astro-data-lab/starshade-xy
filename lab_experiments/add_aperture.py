a87987a60934969b44586b8645366aaa0fae57a2
harnessa <aharness@princeton.edu>
2021-04-12 14:24:39 -0400

    updated add_aperture.py

--- lab_experiments/add_aperture.py
+++ lab_experiments/add_aperture.py
@@ -14,55 +14,32 @@ import matplotlib.pyplot as plt;plt.ion()
 from astropy.io import fits
 import h5py
 import glob
+import image_util
 from scipy.ndimage import affine_transform
 
 #Choose aperture size for proper scaling to space
 num_pts = 96
+image_pad = 10
 
 #aperture size (from pupil magnification)
 Dtel = num_pts * 1.748*13e-6
 
+session = 'new_data_30s'
+
 #TODO: hide
-data_dir = '/home/aharness/Research/Frick_Lab/Data/FFNN/data'
+data_dir = f'/home/aharness/Research/Frick_Lab/Data/FFNN/{session}'
 
 #Read record
 record = np.genfromtxt(f'{data_dir}/record.csv', delimiter=',')
 
-def get_image(fname):
-    with fits.open(fname) as hdu:
-        data = hdu[0].data
-        utmp = hdu[0].header['UNSTTEMP']
-    return data, utmp
-
-#Get background image
-with fits.open(f'{data_dir}/background.fits') as hdu:
-    back = np.median(hdu[0].data, 0)
+def get_image(inum):
+    with fits.open(f'{data_dir}/image__{str(inum).zfill(4)}.fits') as hdu:
+        data = hdu[0].data.astype(float)
+    return data
 
 #Get image shape
-img_shp = back.shape
-
-#Loop through steps and get images
-imgs = np.empty((0,) + img_shp)
-locs = np.empty((0, 2))
-for i in range(len(record))[450:]:
-
-    #Current step number
-    stp = str(int(record[i][0])).zfill(4)
-
-    #Current position
-    pos = record[i][1:]
-
-    #Get image
-    img, utmp = get_image(f'{data_dir}/image__{stp}.fits')
-    print(np.median(img), utmp)
-    breakpoint()
-
-    #Store images + positions
-    imgs = np.concatenate((imgs, img))
-    locs = np.concatenate((locs, [pos]*img.shape[0]))
-
-#Subtract background
-# imgs -= back
+img0 = get_image(len(record)//2)
+img_shp = img0.shape[-2:]
 
 #Load Pupil Mask
 with h5py.File(f'../diffraction_code/xtras/pupil_mask.h5', 'r') as f:
@@ -74,11 +51,71 @@ dx = -scaling*(img_shp[0]-num_pts)/2
 affmat = np.array([[scaling, 0, dx], [0, scaling, dx]])
 pupil_mask = affine_transform(full_mask, affmat, output_shape=img_shp, order=5)
 
-#Add bounds
-pupil_mask[pupil_mask < 0] = 0
-pupil_mask[pupil_mask > 1] = 1
+#Make binary
+pupil_mask[pupil_mask <  0.5] = 0
+pupil_mask[pupil_mask >= 0.5] = 1
 
-#Apply  mask
+#Get indices of spiders and outside aperture
+rr = np.hypot(*(np.indices(img_shp) - img_shp[0]/2))
+spiders = (rr <= num_pts/2) & (pupil_mask == 0)
+out_mask = rr > num_pts/2
+nspid = np.count_nonzero(spiders)
+nout = np.count_nonzero(out_mask)
 
-plt.imshow(imgs[0])
+#Tile for multiple exposures
+spiders = np.tile(spiders, (img0.shape[0], 1, 1))
+out_mask = np.tile(out_mask, (img0.shape[0], 1, 1))
+
+#Loop through steps and get images
+imgs = np.empty((0,) + img_shp)
+locs = np.empty((0, 2))
+for i in range(len(record)):
+
+    #Current position
+    pos = record[i][1:]
+
+    #Get image
+    img = get_image(int(record[i][0]))
+
+    #Get excess regions
+    excess = np.concatenate((img[:,:50,:50], img[:,:50,-50:], \
+        img[:,-50:,:50], img[:,-50:,-50:])).flatten()
+
+    #Add mask
+    img *= pupil_mask
+
+    #Add noise in spiders and outside aperture
+    img[spiders] = np.random.choice(excess, (img.shape[0], nspid)).flatten()
+    img[out_mask] = np.random.choice(excess, (img.shape[0], nout)).flatten()
+
+    #Subtract background
+    img -= np.median(excess)
+
+    #Plot
+    if [False, True][0]:
+        print(i, pos*1e3)
+        for j in range(img.shape[0]):
+            plt.cla()
+            plt.imshow(img[j])
+            breakpoint()
+
+    #Store images + positions
+    imgs = np.concatenate((imgs, img))
+    locs = np.concatenate((locs, [pos]*img.shape[0]))
+
+#Trim images
+imgs = image_util.crop_image(imgs, None, num_pts//2+image_pad)
+
+plt.cla()
+plt.imshow(imgs[len(imgs)//2])
+
+#Save
+if [False, True][0]:
+    with h5py.File(f'./Results/{session}.h5', 'w') as f:
+        f.create_dataset('num_tel_pts', data=num_pts)
+        f.create_dataset('image_pad', data=image_pad)
+        f.create_dataset('tel_diameter', data=Dtel)
+        f.create_dataset('positions', data=locs, compression=8)
+        f.create_dataset('images', data=imgs, compression=8)
+
 breakpoint()

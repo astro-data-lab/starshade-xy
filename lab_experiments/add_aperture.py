da3ef1c283520c58c3495d717d025703aca09b05
harnessa <aharness@princeton.edu>
2021-04-19 08:58:36 -0400

    added estimate_snr

--- lab_experiments/add_aperture.py
+++ lab_experiments/add_aperture.py
@@ -17,122 +17,129 @@ import glob
 import image_util
 from scipy.ndimage import affine_transform
 
-session = ['new_data_30s', 'data_30s_bin2', 'data_20s_bin4', 'data_60s_bin4'][3]
+all_sessions = ['data_30s_bin1', 'data_30s_bin2', 'data_20s_bin4', \
+    'data_60s_bin4']
 
 is_med = True
+do_save = True
 
-#Choose aperture size for proper scaling to space
-num_pts = 96
-image_pad = 10
+for session in all_sessions:
 
-#aperture size (from pupil magnification)
-Dtel = num_pts * 1.748*13e-6
+    #Choose aperture size for proper scaling to space
+    num_pts = 96
+    image_pad = 10
 
-#TODO: hide
-data_dir = f'/home/aharness/Research/Frick_Lab/Data/FFNN/{session}'
+    #aperture size (from pupil magnification)
+    Dtel = num_pts * 1.748*13e-6
 
-#Read record
-record = np.genfromtxt(f'{data_dir}/record.csv', delimiter=',')
+    #TODO: hide
+    data_dir = f'/home/aharness/Research/Frick_Lab/Data/FFNN/{session}'
 
-def get_image(inum):
-    with fits.open(f'{data_dir}/image__{str(inum).zfill(4)}.fits') as hdu:
-        data = hdu[0].data.astype(float)
-    return data
+    #Read record
+    record = np.genfromtxt(f'{data_dir}/record.csv', delimiter=',')
 
-#Get image shape
-img0 = get_image(len(record)//2)
-img_shp = img0.shape[-2:]
+    def get_image(inum):
+        with fits.open(f'{data_dir}/image__{str(inum).zfill(4)}.fits') as hdu:
+            data = hdu[0].data.astype(float)
+        return data
 
-#Get binning
-nbin = int(np.round(250/img_shp[0]))
-num_pts //= nbin
+    #Get image shape
+    img0 = get_image(len(record)//2)
+    img_shp = img0.shape[-2:]
 
-#Load Pupil Mask
-with h5py.File(f'../diffraction_code/xtras/pupil_mask.h5', 'r') as f:
-    full_mask = f['mask'][()]
+    #Get binning
+    nbin = int(np.round(250/img_shp[0]))
+    num_pts //= nbin
 
-#Do affine transform
-scaling = full_mask.shape[0]/num_pts
-dx = -scaling*(img_shp[0]-num_pts)/2
-affmat = np.array([[scaling, 0, dx], [0, scaling, dx]])
-pupil_mask = affine_transform(full_mask, affmat, output_shape=img_shp, order=5)
+    #Load Pupil Mask
+    with h5py.File(f'../diffraction_code/xtras/pupil_mask.h5', 'r') as f:
+        full_mask = f['mask'][()]
 
-#Make binary
-pupil_mask[pupil_mask <  0.5] = 0
-pupil_mask[pupil_mask >= 0.5] = 1
+    #Do affine transform
+    scaling = full_mask.shape[0]/num_pts
+    dx = -scaling*(img_shp[0]-num_pts)/2
+    affmat = np.array([[scaling, 0, dx], [0, scaling, dx]])
+    pupil_mask = affine_transform(full_mask, affmat, output_shape=img_shp, order=5)
 
-#Get indices of spiders and outside aperture
-rr = np.hypot(*(np.indices(img_shp) - img_shp[0]/2))
-spiders = (rr <= num_pts/2) & (pupil_mask == 0)
-out_mask = rr > num_pts/2
-nspid = np.count_nonzero(spiders)
-nout = np.count_nonzero(out_mask)
+    #Make binary
+    pupil_mask[pupil_mask <  0.5] = 0
+    pupil_mask[pupil_mask >= 0.5] = 1
 
-#Tile for multiple exposures
-spiders = np.tile(spiders, (img0.shape[0], 1, 1))
-out_mask = np.tile(out_mask, (img0.shape[0], 1, 1))
+    #Get indices of spiders and outside aperture
+    rr = np.hypot(*(np.indices(img_shp) - img_shp[0]/2))
+    spiders = (rr <= num_pts/2) & (pupil_mask == 0)
+    out_mask = rr > num_pts/2
+    nspid = np.count_nonzero(spiders)
+    nout = np.count_nonzero(out_mask)
 
-#Loop through steps and get images
-imgs = np.empty((0,) + img_shp)
-locs = np.empty((0, 2))
-for i in range(len(record)):
+    #Tile for multiple exposures
+    spiders = np.tile(spiders, (img0.shape[0], 1, 1))
+    out_mask = np.tile(out_mask, (img0.shape[0], 1, 1))
 
-    #Current position
-    pos = record[i][1:]
+    #Loop through steps and get images
+    imgs = np.empty((0,) + img_shp)
+    locs = np.empty((0, 2))
+    for i in range(len(record)):
 
-    #Get image
-    img = get_image(int(record[i][0]))
+        #Current position
+        pos = record[i][1:]
 
-    #Get excess regions
-    nbk = 40//nbin
-    excess = np.concatenate((img[:,:nbk,:nbk], img[:,:nbk,-nbk:], \
-        img[:,-nbk:,:nbk], img[:,-nbk:,-nbk:])).flatten()
+        #Get image
+        img = get_image(int(record[i][0]))
 
-    #Add mask
-    img *= pupil_mask
+        #Get excess regions
+        nbk = 40//nbin
+        excess = np.concatenate((img[:,:nbk,:nbk], img[:,:nbk,-nbk:], \
+            img[:,-nbk:,:nbk], img[:,-nbk:,-nbk:])).flatten()
 
-    #Add noise in spiders and outside aperture
-    img[spiders] = np.random.choice(excess, (img.shape[0], nspid)).flatten()
-    img[out_mask] = np.random.choice(excess, (img.shape[0], nout)).flatten()
+        #Add mask
+        img *= pupil_mask
 
-    #Subtract background
-    img -= np.median(excess)
+        #Add noise in spiders and outside aperture
+        img[spiders] = np.random.choice(excess, (img.shape[0], nspid)).flatten()
+        img[out_mask] = np.random.choice(excess, (img.shape[0], nout)).flatten()
 
-    #Take median
-    if is_med:
-        img = np.array([np.median(img, 0)])
+        #Subtract background
+        img -= np.median(excess)
 
-    #Plot
-    if [False, True][0]:
-        print(i, pos*1e3)
-        for j in range(img.shape[0]):
-            plt.cla()
-            # plt.imshow(img[j])
-            cimg = image_util.crop_image(img[j], None, num_pts//2+image_pad)
-            plt.imshow(cimg)
-            print(num_pts, cimg.shape)
-            breakpoint()
+        #Take median
+        if is_med:
+            img = np.array([np.median(img, 0)])
 
-    #Store images + positions
-    imgs = np.concatenate((imgs, img))
-    locs = np.concatenate((locs, [pos]*img.shape[0]))
+        #Plot
+        if [False, True][1]:
+            print(i, pos*1e3)
+            for j in range(img.shape[0]):
+                plt.cla()
+                # plt.imshow(img[j])
+                cimg = image_util.crop_image(img[j], None, num_pts//2+image_pad)
+                plt.imshow(cimg)
+                print(num_pts, cimg.shape)
+                breakpoint()
 
-#Trim images
-imgs = image_util.crop_image(imgs, None, num_pts//2+image_pad)
+        #Store images + positions
+        imgs = np.concatenate((imgs, img))
+        locs = np.concatenate((locs, [pos]*img.shape[0]))
 
-plt.cla()
-plt.imshow(imgs[len(imgs)//2])
+    #Trim images
+    imgs = image_util.crop_image(imgs, None, num_pts//2+image_pad)
 
-#Save
-if [False, True][1]:
+    #Flip sign of x-coord b/c optics
+    locs[:,0] *= -1
 
-    ext = ['', '__median'][int(is_med)]
+    plt.cla()
+    plt.imshow(imgs[len(imgs)//2])
 
-    with h5py.File(f'./Results/{session}{ext}.h5', 'w') as f:
-        f.create_dataset('num_tel_pts', data=num_pts)
-        f.create_dataset('image_pad', data=image_pad)
-        f.create_dataset('tel_diameter', data=Dtel)
-        f.create_dataset('positions', data=locs, compression=8)
-        f.create_dataset('images', data=imgs, compression=8)
+    #Save
+    if do_save:
+
+        ext = ['', '__median'][int(is_med)]
+
+        with h5py.File(f'./Results/{session}{ext}.h5', 'w') as f:
+            f.create_dataset('num_tel_pts', data=num_pts)
+            f.create_dataset('image_pad', data=image_pad)
+            f.create_dataset('tel_diameter', data=Dtel)
+            f.create_dataset('positions', data=locs, compression=8)
+            f.create_dataset('images', data=imgs, compression=8)
 
 breakpoint()
